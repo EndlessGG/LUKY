e51cee693c86fa4ed933362b24ae74d35220dc11
EpikGhost <utp0147318@alumno.utpuebla.edu.mx>
2024-07-13 19:11:50 -0600

    Paginas bases del proyecto LUKY

--- resources/public/js/index.js
+++ resources/public/js/index.js
@@ -1,6 +1,11 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const titleElement = document.getElementById('title');
-    if (titleElement) {
-        titleElement.style.color = 'blue';
-    }
+const container = document.querySelector(".container");
+const btnSignIn = document.getElementById("btn-sign-in");
+const btnSignUp = document.getElementById("btn-sign-up");
+
+btnSignIn.addEventListener("click", () => {
+   container.classList.remove("toggle");
+});
+
+btnSignUp.addEventListener("click", () => {
+   container.classList.add("toggle");
 });
