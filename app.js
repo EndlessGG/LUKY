fc9ca1467304c87f26c339faf5cc25f7e8393d72
Juan David Parra Santizo <johnsonsantizo26042003@gmail.com>
2024-07-15 23:20:15 -0600

    Registro e Inicio de sesion, junto al cierre se sesion y obtencion de datos el usuario - BackEnd

--- app.js
+++ app.js
@@ -5,22 +5,33 @@ const app = express()
 require('dotenv').config()
 const frontendRoutes = require('./app/routes/front.routes')
 const dbConnection = require('./config/database')
+const authRoutes = require('./app/routes/auth.routes')
+const cookieParser = require('cookie-parser')
+const sessionMiddleware = require('./app/middlewares/sessionMiddleware')
+const jwt = require('jsonwebtoken')
 
 const PORT = process.env.PORT || 3000
-
 
 //motor de vistas ejs
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'resources/views'))
 app.use(express.static(path.join(__dirname, 'resources/public'))) // archivos estaticos
 
-
 //middleware
 app.use(express.json())
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false })) // para los formularios y mas
+app.use(cookieParser())
+app.use(sessionMiddleware)
+
 
 //endpoints
-app.use('/', frontendRoutes) // rutas de paginas
+app.use('/', frontendRoutes) // rutas de paginas y renderizar las vistas de leo
+app.use('/auth', authRoutes) // para las peticiones de autenticacion
+app.post('/api/test-cookie', (req, res) => { // este despues se quita solo es prueba
+    console.log('Cookies en la solicitud de prueba:', req.cookies)
+    res.json({ cookies: req.cookies })
+})
 
 
 app.listen(PORT, () => {
---
+++ app/Models/user.js
@@ -0,0 +1,162 @@
+const dbConnection = require('../../config/database')
+const bcrypt = require('bcrypt')
+
+class User {
+    constructor(nombres, apellidoPaterno, apellidoMaterno, email, pass, telefono, isTrabajador) {
+        this.nombres = nombres
+        this.apellidoPaterno = apellidoPaterno
+        this.apellidoMaterno = apellidoMaterno
+        this.email = email
+        this.pass = pass
+        this.telefono = telefono
+        this.isTrabajador = isTrabajador
+    }
+
+    static async hashPassword(password) {
+        try {
+            const hashedPassword = await bcrypt.hash(password, 10)
+            return hashedPassword
+        } catch (error) {
+            console.error('Error al hashear password, model:', error)
+            throw error
+        }
+    }
+
+    static async comparePassword(password, hashedPassword) {
+        try {
+            const isMatch = await bcrypt.compare(password, hashedPassword)
+            return isMatch
+        } catch (error) {
+            console.error('Error al cmoparar password, model:', error)
+            throw error
+        }
+    }
+
+    async registerUser() {
+        try {
+            const [result] = await dbConnection.query(
+                'INSERT INTO usuarios (nombres, apellidoPaterno, apellidoMaterno, email, pass, telefono, isTrabajador) VALUES (?, ?, ?, ?, ?, ?, ?)',
+                [
+                    this.nombres,
+                    this.apellidoPaterno,
+                    this.apellidoMaterno,
+                    this.email,
+                    this.pass,
+                    this.telefono,
+                    this.isTrabajador
+                ]
+            )
+            return result
+        } catch (error) {
+            console.error('Error al registrar nuevo usuario, model:', error)
+            throw error
+        }
+    }
+
+    static async findUserByEmail(email) {
+        try {
+            const [rows] = await dbConnection.query(
+                'SELECT ID, email, pass FROM usuarios WHERE email = ?',
+                [email]
+            )
+            return rows[0]
+        } catch (error) {
+            console.error('Error al obtener correo, model:', error)
+            throw error
+        }
+    }
+
+    static async getAllInfoUser(email) {
+        try {
+            const [rows] = await dbConnection.query(
+                'SELECT * FROM usuarios WHERE email = ?',
+                [email]
+            )
+            return rows[0]
+        } catch (error) {
+            console.error('Error al obtener informacion de usuario, model:', error)
+            throw error
+        }
+    }
+}
+
+module.exports = User
+
+// // Encriptacion de Contrasenas
+// const hashPassword = async (password) => {
+//     try {
+//         const hashedPassword = await bcrypt.hash(password, 10)
+//         return hashedPassword
+//     } catch (error) {
+//         console.error('Error al hashear password, model:', error)
+//         throw error
+//     }
+// }
+
+// // Comparacion de Contrasena Encriptada con la proporcionada
+// const comparePassword = async (password, hashedPassword) => {
+//     try {
+//         const isMatch = await bcrypt.compare(password, hashedPassword)
+//         return isMatch
+//     } catch (error) {
+//         console.error('Error al cmoparar password, model:', error)
+//         throw error
+//     }
+// }
+
+// // Registro de Usuarios
+// const registerUser = async ({ nombres, apellidoPaterno, apellidoMaterno, email, pass, telefono, isTrabajador }) => {
+//     try {
+//         const [result] = await dbConnection.query(
+//             'INSERT INTO usuarios (nombres, apellidoPaterno, apellidoMaterno, email, pass, telefono, isTrabajador) VALUES (?, ?, ?, ?, ?, ?, ?)',
+//             [nombres, apellidoPaterno, apellidoMaterno, email, pass, telefono, isTrabajador]
+//         )
+//         return result
+//     } catch (error) {
+//         console.error('Error al registrar nuevo usuario, model:', error)
+//         throw error
+//     }
+// }
+
+// // Buscar usuario por username v:
+// const findUsername = async (username) => { // TODO: arreglar
+//     try {
+//         const [rows] = await dbConnection.query(
+//             'SELECT * FROM usuarios WHERE username = ?',
+//             [username]
+//         )
+//         return rows[0]
+//     } catch (error) {
+//         console.error('Error al obtener usuario, model:', error)
+//         throw error
+//     }
+// }
+
+// // Buscar usuario por correo
+// const findEmail = async (email) => {
+//     try {
+//         const [rows] = await dbConnection.query(
+//             'SELECT ID, email, pass FROM usuarios WHERE email = ?',
+//             [email]
+//         )
+//         return rows[0]
+//     } catch (error) {
+//         console.error('Error al obtener correo, model:', error)
+//         throw error
+//     }
+// }
+
+// const getAllInfoUser = async (email) => {
+//     try {
+//         const [rows] = await dbConnection.query(
+//             'SELECT * FROM usuarios WHERE email = ?',
+//             [email]
+//         )
+//         return rows[0]
+//     } catch (error) {
+//         console.error('Error al obtener informacion de usuario, model:', error)
+//         throw error
+//     }
+// }
+
+// module.exports = { hashPassword, comparePassword, registerUser, findEmail, findUsername, getAllInfoUser }
