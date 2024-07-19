d5c78b0e543fddc2ce14044a5833ef1a2edd7e2a
EpikGhost <utp0147318@alumno.utpuebla.edu.mx>
2024-07-19 12:48:01 -0600

    Ultimos cambios, y archivos agregados
    Ya seria casi todo por parte del frontend

--- app/routes/front.routes.js
+++ app/routes/front.routes.js
@@ -7,6 +7,6 @@ router.get('/Formulario', frontController.showFormPage) // pagina de inicio
 router.get('/Perfil', frontController.showPerfilPage) // pagina de inicio
 router.get('/Busqueda', frontController.showBusquedaPage) // pagina de inicio
 router.get('/Terminos', frontController.showTerminosPage) // pagina de inicio
-//router.get('/Acerca', frontController.showAcercap) // pagina de inicio
+router.get('/Acerca', frontController.showAcercaPage) // pagina de inicio
 
 module.exports = router
