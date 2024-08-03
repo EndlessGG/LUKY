59b33df6c8f935cf2f4397c963db25fad77ae43f
Juan David Parra Santizo <johnsonsantizo26042003@gmail.com>
2024-08-02 18:06:21 -0600

    renderizado de datos de usuario

--- app/Controllers/front.controller.js
+++ app/Controllers/front.controller.js
@@ -1,5 +1,8 @@
 const passport = require('passport')
 const path = require('path')
+const User = require('../Models/user')
+const genToken = require('../middlewares/tokenMiddleware')
+const moment = require('moment')
 
 exports.showIndexPage = (req, res) => {
     res.render('index')  // pagina de inicio
@@ -7,9 +10,35 @@ exports.showIndexPage = (req, res) => {
 exports.showFormPage = (req, res) => {
     res.render('formulario', { errorEmail_l: null, email: '', errorPassword_l: null })  // pagina de formulario
 }
-exports.showPerfilPage = (req, res) => {
-    res.render('perfil')  // pagina de perfil
+
+exports.showPerfilPage = async (req, res) => {
+    try {
+        const token = req.cookies.token
+        const infoUsuario = genToken.extraerInfoToken(token)
+
+        if (!infoUsuario) {
+            return res.status(401).json({ mensaje: 'Token inválido' })
+        }
+
+        req.usuario = infoUsuario
+        const email = req.usuario.email
+
+        const user = await User.getAllInfoUser(email)
+
+        if (user) {
+            const { pass, ...userWithoutPassword } = user;
+            // Formatear la fecha
+            userWithoutPassword.fechaCreacion = moment(userWithoutPassword.fechaCreacion).format('DD/MM/YYYY')
+            res.render('perfil', { user: userWithoutPassword });
+        } else {
+            res.status(404).json({ message: 'Usuario no encontrado' })
+        }
+    } catch (error) {
+        console.error('Error al obtener datos del usuario, controller:', error)
+        res.status(500).json({ message: 'Error interno del servidor' })
+    }
 }
+
 exports.showBusquedaPage = (req, res) => {
     const { termino } = req.query
     let publicaciones = []
